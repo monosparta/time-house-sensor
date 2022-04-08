ebd99b4a9b20809dbb08e506550fbea4c5a9b8e8
Hong-Shao <joee0511@gmail.com>
2022-04-08 16:26:10 +0800

    fix: :bug: add db config.js (dev)

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,9 +1,9 @@
-{
+module.exports = {
   "development": {
-    "username": "root",
-    "password": "user",
-    "database": "dev",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {

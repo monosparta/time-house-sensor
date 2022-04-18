1575cc6189e31c05f57e951056654b6bced84c94
20181101remon <mindy80230@gmail.com>
2022-04-18 17:05:49 +0800

    登入刻板v1

--- frontend/my-app/src/pages/Login.js
+++ frontend/my-app/src/pages/Login.js
@@ -36,80 +36,80 @@ const Login = () => {
         <div>
             <Row>
                 <Col span={8} className="Color box">
-
-                    <h2 style={{color:"while"}}  >時光屋座位管理系統</h2>
+                    <div className='logo'>
+                    <h2 style={{ color: "while" }}  >時光屋座位管理系統</h2>
                     <img src={"../image/logo.png"} alt="logo"></img>
+                    </div>
+                  
                 </Col>
                 <Col span={16}>
                     <div class="box">
                         <div class="content">
-                            <Form
-                                name="basic"
-                                labelCol={{
-                                    span: 5,
-                                }}
-                                wrapperCol={{
-                                    span: 19,
-                                }}
-                                initialValues={{
-                                    remember: true,
-                                }}
-                                onFinish={(e) => { onFinish(e); e.preventDefault(); }}
-                                // useForm.js:884 TypeError: e.preventDefault is not a function
-                                // https://ithelp.ithome.com.tw/articles/10245434
-                                onFinishFailed={onFinishFailed}
-                                autoComplete="off"
-                            >
-                                <Form.Item
-                                    label="name"
-                                    name="name"
-                                    value={name}
-                                    rules={[
-                                        {
-                                            required: true,
-                                            message: 'Please input your username!',
-                                        },
-                                    ]}
-                                >
-                                    <Input />
-                                </Form.Item>
-                                <Form.Item
-                                    label="pwd"
-                                    name="pwd"
-                                    value={pwd}
-                                    // onChange={(e)=>setName(e.target.value)}
-                                    rules={[
-                                        {
-                                            required: true,
-                                            message: 'Please input your password!',
-                                        },
-                                    ]}
-                                >
-                                    <Input.Password />
-                                </Form.Item>
+                            <div class="form">
+                                <h3>Sign In</h3>
+                                <Form
 
-                                <Form.Item
-                                    name="remember"
-                                    valuePropName="checked"
-                                    wrapperCol={{
-                                        offset: 8,
-                                        span: 16,
+                                    name="basic"
+                                    labelCol={{
+                                        span: 24,
                                     }}
+                                    wrapperCol={{
+                                        span: 24,
+                                    }}
+                                    initialValues={{
+                                        remember: true,
+                                    }}
+                                    onFinish={(e) => { onFinish(e); e.preventDefault(); }}
+                                    // useForm.js:884 TypeError: e.preventDefault is not a function
+                                    // https://ithelp.ithome.com.tw/articles/10245434
+                                    onFinishFailed={onFinishFailed}
+                                    autoComplete="off"
                                 >
-                                    <Checkbox>Remember me</Checkbox>
-                                </Form.Item>
+                                    <Form.Item
+                                        label="帳號 Usernamer or Email"
+                                        name="name"
+                                        value={name}
+                                        rules={[
+                                            {
+                                                required: true,
+                                                message: 'Please input your username!',
+                                            },
+                                        ]}
+                                    >
+                                        <Input placeholder="請輸入帳號"/>
+                                    </Form.Item>
+                                    <Form.Item
+                                        label="密碼 Password"
+                                        name="pwd"
+                                        value={pwd}
+                                        rules={[
+                                            {
+                                                required: true,
+                                                message: 'Please input your password!',
+                                            },
+                                        ]}
+                                    >
+                                        <Input.Password placeholder="請輸入密碼"/>
+                                    </Form.Item>
 
-                                <Form.Item
-                                    wrapperCol={{
-                                        offset: 8,
-                                        span: 16,
-                                    }}
-                                >
-                                    <Button type="primary" htmlType="submit">
-                                        Submit
-                                    </Button>
-                                </Form.Item>
-                            </Form>
+                                    <Form.Item
+                                        name="remember"
+                                        valuePropName="checked"
+                                        offset="1"
+                                    >
+                                        <Checkbox  className='checkbox-red'>保持登入</Checkbox>
+                                    </Form.Item>
+
+                                    <Form.Item
+
+                                    >
+                                        <Button type="primary" htmlType="submit" block  size='large' style={{ background: "#363F4E", borderColor: "#363F4E" }}>
+                                            立即登入
+                                        </Button>
+                                    </Form.Item>
+                                </Form>
+                            </div>
+
                         </div>
                     </div>
                 </Col>

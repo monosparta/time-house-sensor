b4494d024f117281a1e7b859e70fb74f7bc7894a
20181101remon <mindy80230@gmail.com>
2022-04-20 17:10:12 +0800

    config

--- frontend/my-app/src/Axios.config.js
+++ frontend/my-app/src/Axios.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-20 13:50:21
- * @LastEditTime: 2022-04-20 13:55:25
+ * @LastEditTime: 2022-04-20 16:48:10
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \time-house-sensor\frontend\my-app\src\Axios.config.js
@@ -9,7 +9,7 @@
 import Axios from "axios"
 const axios = (baseURL) => {
 const instance = Axios.create({
-baseURL: 'https://2a83-211-72-239-241.ngrok.io',//back-end
+baseURL: 'http://localhost:3000',//back-end
 headers: { 'Content-Type': 'application/json' },
 timeout: 6000,
 });

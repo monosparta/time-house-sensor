44f38c4294b8d9e120e283ec84d8c4df819a3ff3
Hong-Shao <joee0511@gmail.com>
2022-04-26 14:12:36 +0800

    remove partial type check

--- backend/src/services/seat.js
+++ backend/src/services/seat.js
@@ -1,35 +1,14 @@
 const db = require("../models/index");
 
-const updateSeatState = async (index, state, memberId) => {
-  try {
-    if (
-      typeof index !== "number" ||
-      typeof state !== "number" ||
-      typeof memberId !== "number"
-    ) {
-      throw new TypeError("Index, State, Member-ID must be number");
-    }
-    if (
-      index < 1 ||
-      index > 10 ||
-      !Number.isInteger(index) ||
-      state < -1 ||
-      state > 2 ||
-      !Number.isInteger(state)
-    ) {
-      throw new RangeError("Index or State is out of range or not an integer");
-    }
-    if (state === 1) {
-      memberId = null;
-    }
-    const seat = await db["Seats"].update(
-      { state: state, memberId: memberId },
-      { where: { id: index } }
-    );
-    return "success";
-  } catch (err) {
-    return err;
+const updateSeatState = async (index, state, memberId, updateTime=new Date()) => {
+  if (state === 1) {
+    memberId = null;
   }
+  const seat = await db["Seats"].update(
+    { state: state, memberId: memberId, updatedAt: updateTime},
+    { where: { id: index } }
+  );
+  return "success";
 };
 
 const getAllSeatInfo = async () => {
@@ -40,9 +19,14 @@ const getAllSeatInfo = async () => {
   return seats;
 };
 
+const getOneSeatInfo = async (index) => {
+  let seats = await db["Seats"].findOne({ where: { id: index } });
+  return seats.dataValues;
+};
+
 const checkSeatIndexExist = async (index) => {
   let seat = await db["Seats"].findOne({
-    where: { id: index }
+    where: { id: index },
   });
   return seat !== null;
 };
@@ -50,5 +34,6 @@ const checkSeatIndexExist = async (index) => {
 module.exports = {
   updateSeatState,
   getAllSeatInfo,
-  checkSeatIndexExist
+  getOneSeatInfo,
+  checkSeatIndexExist,
 };

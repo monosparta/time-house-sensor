e1700c3b69c01f315780cf63b3599090cd2608f8
Hong-Shao <joee0511@gmail.com>
2022-04-28 09:56:56 +0800

    fix: :bug: not to change seat state to IDLE_TOO_LONG, when nobody uses it

--- backend/src/controllers/mosquitto.js
+++ backend/src/controllers/mosquitto.js
@@ -5,17 +5,21 @@ const seatState = require("../utils/seatState");
  *
  * @param {seatUseState} 0: nobody, 1: someone
  */
-const IRHandler = async ({ index, seatUseState, time }) => {
+const IRHandler = async ({ index, seatUseState }) => {
   const seat = await seatService.getOneSeatInfo(index);
   if (seatUseState === 0) {
+    if (seat.state === 1) {
+      return;
+    }
     seatService.updateSeatState(index, seatState.IDLE_TOO_LONG, seat.memberId);
     lineDev.pushAdminMessage(index);
+    return;
   } else if (seatUseState === 1) {
     seatService.updateSeatState(index, seatState.USING, seat.memberId);
   }
 };
 
-const RFIDHandler = async ({ index, cardId, time }) => {
+const RFIDHandler = async ({ index, cardId }) => {
   const memberInfo = await memberService.getMemberInfoByCardId(cardId);
   if (!memberInfo) {
     return;
@@ -23,9 +27,15 @@ const RFIDHandler = async ({ index, cardId, time }) => {
   seatService.updateSeatState(index, seatState.USING, memberInfo.id);
 };
 
-const errorHandler = async ({ index, errorMessage, sensorName, time }) => {
+const errorHandler = async ({ index, errorMessage, sensorName }) => {
   seatService.updateSeatState(index, seatState.ERROR, null);
 };
+
+// errorHandler({index: 1, errorMessage: "", sensorName: "", time: new Date()})
+
+// RFIDHandler({index: 1, cardId: "0123456789", time: new Date()});
+
+// IRHandler({index: 1, seatUseState: 1, time: new Date()})
 
 module.exports = {
   IRHandler,

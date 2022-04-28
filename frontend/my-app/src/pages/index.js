08602ae11eacd108365e90b0e9402d7696ea7102
20181101remon <mindy80230@gmail.com>
2022-04-28 16:33:20 +0800

    更新座位刷新時間

--- frontend/my-app/src/pages/index.js
+++ frontend/my-app/src/pages/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-12 12:01:23
- * @LastEditTime: 2022-04-27 16:07:22
+ * @LastEditTime: 2022-04-28 16:29:55
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \time-house-sensor\frontend\my-app\src\pages\index.js
@@ -430,7 +430,7 @@ const Home = () => {
         callSeatApi();
         let timer = setInterval(() => {
             callSeatApi();
-        }, 10000)
+        }, 5000)
         return () => clearInterval(timer);
     }, []);
 

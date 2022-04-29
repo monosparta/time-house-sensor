8fe5f5d8a09d5ed954234630a2e755b79cf0aab5
Hong-Shao <joee0511@gmail.com>
2022-04-29 22:55:47 +0800

    delete deprecated service about grpc

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -1,11 +1,9 @@
 const lineDev = require('./lineDev');
 const member = require('./member');
 const seat = require('./seat');
-const drawer = require('./openCVClient');
 
 module.exports = {
     lineDevService: lineDev,
     memberService: member,
     seatService: seat,
-    drawerService: drawer
 }

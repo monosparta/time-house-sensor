4740250f4efdf70c45eb2058a90bca25e6ad96b6
Hong-Shao <joee0511@gmail.com>
2022-04-29 14:23:29 +0800

    change the state into a readable state

--- backend/src/controllers/mosquitto.js
+++ backend/src/controllers/mosquitto.js
@@ -8,11 +8,16 @@ const seatState = require("../utils/seatState");
 const IRHandler = async ({ index, seatUseState }) => {
   const seat = await seatService.getOneSeatInfo(index);
   if (seatUseState === 0) {
-    if (seat.state === 1) {
+    if (seat.state === seatState.AVAILABLE) {
       return;
     }
-    const idleTime = new Date(new Date().getTime() - 1000*60*30);
-    seatService.updateSeatState(index, seatState.IDLE_TOO_LONG, seat.memberId, idleTime);
+    const idleTime = new Date(new Date().getTime() - 1000 * 60 * 30);
+    seatService.updateSeatState(
+      index,
+      seatState.IDLE_TOO_LONG,
+      seat.memberId,
+      idleTime
+    );
     lineDev.pushAdminMessage(index);
     return;
   } else if (seatUseState === 1) {
@@ -31,7 +36,6 @@ const RFIDHandler = async ({ index, cardId }) => {
 const errorHandler = async ({ index, errorMessage, sensorName }) => {
   seatService.updateSeatState(index, seatState.ERROR, null);
 };
-
 
 module.exports = {
   IRHandler,

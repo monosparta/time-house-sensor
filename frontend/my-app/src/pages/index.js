374ea4336b526da5aa3475f837edf4ded9a5aad3
20181101remon <mindy80230@gmail.com>
2022-05-04 18:00:42 +0800

    修正css bug

--- frontend/my-app/src/pages/index.js
+++ frontend/my-app/src/pages/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-12 12:01:23
- * @LastEditTime: 2022-05-04 17:46:08
+ * @LastEditTime: 2022-05-04 17:57:07
  * @LastEditors: 20181101remon mindy80230@gmail.com
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \time-house-sensor\frontend\my-app\src\pages\index.js
@@ -324,7 +324,7 @@ const CollectionCreateForm = ({
           </Col>
         </Row>
         </Space>
-        </div>
+        
           <Form.Item label="狀態更改" name="state">
           <Radio.Group onChange={onChange} value={chioces}>
               <Radio value={0}>使用中</Radio>
@@ -338,6 +338,7 @@ const CollectionCreateForm = ({
     
             <Input type="hidden"></Input>
           </Form.Item>
+          </div>
         </Form>
       </Modal>
     );

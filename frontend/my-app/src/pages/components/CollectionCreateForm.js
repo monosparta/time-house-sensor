360e1bfe7c65e0ebd4b7074b4097da52e378c0b0
20181101remon <mindy80230@gmail.com>
2022-05-20 13:40:07 +0800

    修正error messege格式

--- frontend/my-app/src/pages/components/CollectionCreateForm.js
+++ frontend/my-app/src/pages/components/CollectionCreateForm.js
@@ -107,7 +107,7 @@ export const CollectionCreateForm = ({
                 rules={[
                   {
                     required: true,
-                    message: "請輸入聯絡電話",
+                    message: "格式錯誤，請重新輸入!",
                   },
                   ({ getFieldValue }) => ({
                     validator(_, value) {
@@ -133,7 +133,7 @@ export const CollectionCreateForm = ({
                   { type: "email", message: "請輸入有效的郵件地址" },
                   {
                     required: true,
-                    message: "請輸入聯絡用信箱!",
+                    message: "格式錯誤，請重新輸入!",
                   },
                 ]}
               >

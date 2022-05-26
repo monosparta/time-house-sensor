21b42843e1499b89f02cae9b1c602e58bff5a5cf
MING-CHUNLee <mindy80230@gmail.com>
2022-05-26 14:05:56 +0800

    style: 修改header

--- frontend/my-app/src/pages/components/HeaderBar.js
+++ frontend/my-app/src/pages/components/HeaderBar.js
@@ -21,10 +21,11 @@ const handleLogout = (e) => {
   return (
     <div>
       <Header className="black">
-        <Row>
+        <Row  justify="space-between">
           <Col
-           sm={{span:4,push:1}} md={{span:5,push:2}} lg={{span:5,push:1}} xl={{span:5,push:1}} xxl={{span:6,push:1}}
-            style={{
+          //  sm={{span:4,push:1}} md={{span:5,push:2}} lg={{span:5,push:1}} xl={{span:5,push:1}} xxl={{span:6,push:1}}
+             sm={{span:4}} md={{span:5}} lg={{span:5}} xl={{span:5}} xxl={{span:6}} 
+          style={{
               verticalAlign: "middle",
               color: "white",
             }}
@@ -32,21 +33,21 @@ const handleLogout = (e) => {
             高階智能座位管理系統
           </Col>
           <Col
-           sm={{span:2,offset:15}} md={{span:2,offset:16}} lg={{span:2,offset:15}} xl={{span:2,offset:15}} xxl={{span:2,offset:15}}
-          
+          //  sm={{span:2,offset:15}} md={{span:2,offset:16}} lg={{span:2,offset:15}} xl={{span:2,offset:15}} xxl={{span:2,offset:15}}
+          sm={{span:2}} md={{span:2}} lg={{span:2,}} xl={{span:2,}} xxl={{span:2,}}
             style={{
               verticalAlign: "middle",
               color: "white",
             }}
           >
             <Button
-              style={{ background: "#363F4E", color: "white" }}
+               className="logut"
               icon={<LogoutOutlined />}
               onClick={(e) => {
                 handleLogout(e);
               }}
             >
-              LOGOUT
+              <span  style={{ fontSize:"14px"}}>LOGOUT</span>
             </Button>
           </Col>
         </Row>

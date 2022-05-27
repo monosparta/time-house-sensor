d9f0e0a10a6e0d8a3c475ad8b97ca614b6f557ab
Hong-Shao <joee0511@gmail.com>
2022-05-27 14:53:47 +0800

    merge stash

--- backend/src/controllers/mosquitto.js
+++ backend/src/controllers/mosquitto.js
@@ -14,25 +14,26 @@ const IRHandler = async ({ index, seatUseState, time }) => {
     seatService.updateSeatState(
       index,
       seatProperty.state.IDLE_TOO_LONG,
+      new Date(time),
       seat.memberId
     );
     lineDev.pushAdminMessage(index);
     return;
   } else if (seatUseState === 1) {
-    seatService.updateSeatState(index, seatProperty.state.USING, seat.memberId);
+    seatService.updateSeatState(index, seatProperty.USING, new Date(time), seat.memberId);
   }
 };
 
-const RFIDHandler = async ({ index, cardId }) => {
+const RFIDHandler = async ({ index, cardId, time }) => {
   const memberInfo = await memberService.getMemberInfoByCardId(cardId);
   if (!memberInfo) {
     return;
   }
-  seatService.updateSeatState(index, seatProperty.state.USING, memberInfo.id);
+  seatService.updateSeatState(index, seatProperty.USING, new Date(time), memberInfo.id);
 };
 
-const errorHandler = async ({ index, errorMessage, sensorName }) => {
-  seatService.updateSeatState(index, seatProperty.state.ERROR, null);
+const errorHandler = async ({ index, errorMessage, sensorName, time }) => {
+  seatService.updateSeatState(index, seatProperty.ERROR, new Date(time), null);
 };
 
 module.exports = {

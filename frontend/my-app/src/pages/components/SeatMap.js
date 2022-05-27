37365c15498705511556e678d11d7c30aef4c795
Hong-Shao <joee0511@gmail.com>
2022-05-27 17:29:51 +0800

    remove url of images

--- frontend/my-app/src/pages/components/SeatMap.js
+++ frontend/my-app/src/pages/components/SeatMap.js
@@ -179,7 +179,7 @@ export const SeatMap = ({ seat,callSeatApi }) => {
             key={seat.id}
             className="chair"
             src={
-              URL + "/api/static/img/seats/" +
+              "/api/static/img/seats/" +
               seat.id +
               ".png?date=" +
               new Date()

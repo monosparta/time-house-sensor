0e76c55f28a640608eef92a8e05d7ac5b909afa4
20181101remon <mindy80230@gmail.com>
2022-07-22 11:12:57 +0800

    style: :recycle: 修正空格

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-12 11:08:57
- * @LastEditTime: 2022-05-03 11:08:01
+ * @LastEditTime: 2022-06-27 17:16:23
  * @LastEditors: 20181101remon mindy80230@gmail.com
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \time-house-sensor\frontend\my-app\src\App.js
@@ -37,7 +37,7 @@ function App() {
             ) : (
               // <Route path="*" element={<Button>sddsd</Button>} />
               <Routes>
-                   <Route path="/login" index element={<Login />} />
+              <Route path="/login" index element={<Login />} />
               <Route path="*" element={<Navigate to="/login" replace />} />
               </Routes>
             )}

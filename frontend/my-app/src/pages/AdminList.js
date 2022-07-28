b33a2e6b2a85e40b25a0fdf307b330c4cf4ec310
20181101remon <mindy80230@gmail.com>
2022-07-28 16:09:37 +0800

    feat: :sparkles: 使用i18n vscode外掛以及新增德與

--- frontend/my-app/src/pages/AdminList.js
+++ frontend/my-app/src/pages/AdminList.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-12 12:01:23
- * @LastEditTime: 2022-07-27 10:16:36
+ * @LastEditTime: 2022-07-28 15:55:03
  * @LastEditors: 20181101remon mindy80230@gmail.com
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \time-house-sensor\frontend\my-app\src\pages\index.js

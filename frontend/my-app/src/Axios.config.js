2a601edc75ac78e14f04c720b46a29853ce14249
MING-CHUNLee <mindy80230@gmail.com>
2022-08-24 10:01:06 +0800

    style: :recycle: remove unwanted programs

--- frontend/my-app/src/Axios.config.js
+++ frontend/my-app/src/Axios.config.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2022-04-20 13:50:21
- * @LastEditTime: 2022-05-19 17:20:33
- * @LastEditors: 20181101remon mindy80230@gmail.com
+ * @LastEditTime: 2022-08-22 11:05:18
+ * @LastEditors: MING-CHUNLee mindy80230@gmail.com
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \time-house-sensor\frontend\my-app\src\Axios.config.js
  */
@@ -10,14 +10,14 @@ import Axios from "axios";
 const axios = (baseURL) => {
   const instance = Axios.create({
     baseURL:process.env.REACT_APP_BASEURL, 
-    // baseURL: "http://localhost:3000", //back-end
+
 
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials": "true",
     },
-   
+
   });
   return instance;
 };
--- frontend/my-app/src/pages/components/NumericInput.js
+++ frontend/my-app/src/pages/components/NumericInput.js
@@ -1,8 +1,8 @@
 /*
  * @Author: 20181101remon mindy80230@gmail.com
  * @Date: 2022-06-09 11:41:53
- * @LastEditors: 20181101remon mindy80230@gmail.com
- * @LastEditTime: 2022-06-17 15:32:21
+ * @LastEditors: MING-CHUNLee mindy80230@gmail.com
+ * @LastEditTime: 2022-08-22 13:33:44
  * @FilePath: \time-house-sensor\frontend\my-app\src\pages\components\NumericInput.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -40,7 +40,7 @@ export function NumericInput(props) {
          
         maxLength={12}
       />
-      {/* {console.log(props)} */}
+ 
     </div>
   );
 }
